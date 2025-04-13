9f5d8026a9b2a9894aab8761824b98857ea1eff4
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-13 21:49:33 +0300

    add Header, Button and Votes components in part1 unicafe app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Header = ({ header }) => (
+  <>
+    <h1>{header}</h1>
+  </>
+);
+
+const Button = ({ text, onClick }) => (
+  <>
+    <button onClick={onClick}>{text}</button>
+  </>
+);
+
+const Votes = ({label, score}) => <><p>{label} - {score}</p></>
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const headers = ["give feedback", "statistics"];
+  const btnTexts = {
+    good : "good",
+    neutral: "neutral",
+    bad: "bad"
+  }
 
   return (
     <div>
-      code here
+      <Header header={headers[0]} />
+      <Button onClick={() => setGood(good + 1)} text={btnTexts.good} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={btnTexts.neutral} />
+      <Button onClick={() => setBad(bad + 1)} text={btnTexts.bad} />
+      <Header header={headers[1]} />
+      <Votes label={btnTexts.good} score={good} />
+      <Votes label={btnTexts.neutral} score={neutral} />
+      <Votes label={btnTexts.bad} score={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
