df1f45cdac151fb921f5706b9bc86682393d3487
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-15 00:33:44 +0300

    expanded persons by adding a number element in part2 phonebook app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const addName = (event) => {
@@ -15,9 +22,11 @@ const App = () => {
       return alert(`${newName} is already added to phonebook`);
     const nameObj = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(nameObj));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -28,12 +37,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.number}</p>
       ))}
     </div>
   );
