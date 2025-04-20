0a3c6c54ca61bc3fe98326c237fbaede154fc7d5
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-20 22:17:50 +0300

    Add cors to make frontend phonebook app to work with backend in part3 backend server

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("postData", (request) => JSON.stringify(request.body));
@@ -10,6 +11,7 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms :postData"
   )
 );
+app.use(cors());
 
 let persons = [
   {
