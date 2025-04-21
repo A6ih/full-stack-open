1d06ec972f1d1d5a8461c47e274c3b4b6ab0be8b
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-21 20:40:41 +0300

    add validition to check number in a specific format and required lenght and add error message if failed validating in part3 backend server

--- part3/index.js
+++ part3/index.js
@@ -50,14 +50,9 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = request.body;
 
-  if (!name) {
-    return response.status(400).json({
-      error: "name cannot be empty",
-    });
-  }
   if (!number) {
     return response.status(400).json({
       error: "number cannot be empty",
@@ -69,13 +64,17 @@ app.post("/api/persons", (request, response) => {
     number: number,
   });
 
-  person.save().then((newPerson) => {
-    response.json(newPerson);
-  });
+  person
+    .save()
+    .then((newPerson) => {
+      response.json(newPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
   const { name, number } = request.body;
+
   Person.findById(request.params.id)
     .then((person) => {
       if (!person) {
@@ -84,9 +83,12 @@ app.put("/api/persons/:id", (request, response, next) => {
       person.name = name;
       person.number = number;
 
-      person.save().then((updatedPerson) => {
-        response.json(updatedPerson);
-      });
+      person
+        .save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
     })
     .catch((error) => next(error));
 });
@@ -96,6 +98,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
