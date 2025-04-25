6b393759ad9e97d0535f0986347f77462055e439
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-26 00:47:36 +0300

    Refactor the application into separate modules in part4 backend server

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+const url = config.MONGODB_URI
+mongoose
+  .connect(url)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) =>
+    logger.error('Connection to MongoDB failed:', error.message)
+  )
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,45 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  liked: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) =>
-    console.error('connection to MongoDB failed error:', error.message)
-  )
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
