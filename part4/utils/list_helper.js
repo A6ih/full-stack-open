6c87a41d98727a6bd8f399d37512e0d6e086ab5a
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-26 21:55:48 +0300

    Add totalLikes function and totalLikes test unit in part4 backend server

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,12 @@
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((acc, curr) => acc + curr.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
