53a2ddc220c37f61a1cb6f3cc0d6bfda52b7f6c2
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-26 22:14:49 +0300

    Add favoriteBlog function and favoriteBlog test unit in part4 backend server

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
     : blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const copy = [...blogs]
+
+  copy.sort((a, b) => b.likes - a.likes)
+
+  return blogs.length === 0 ? 0 : copy[0]
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
