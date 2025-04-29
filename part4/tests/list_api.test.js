2dcb17ad0129905dd63b58fefdbb02e99bea793c
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-29 23:32:00 +0300

    Add list_api.test.js, add supertest and cross-env packages and refactor router to use async/await in part4 backend server

---
+++ part4/tests/list_api.test.js
@@ -0,0 +1,53 @@
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const blogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(blogs)
+})
+
+describe('returns', () => {
+  test('as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('correct amount of blogs', async () => {
+    const result = await api.get('/api/blogs')
+
+    assert.strictEqual(result.body.length, blogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
