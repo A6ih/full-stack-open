6f1315e15dad9fb1ad9db819b562c0f6202993d3
A6ih <177154114+A6ih@users.noreply.github.com>
2025-05-27 23:35:33 +0300

    Modify adding new blogs so that it is only possible if a valid token is sent with the HTTP POST request. The user identified by the token is designated as the creator of the blog in part4 backend server

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    const blogs = await Blog.find({}).populate('user', {
+      username: 1,
+      name: 1,
+    })
     return response.json(blogs)
   } catch (exception) {
     next(exception)
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if(!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
-    if(!body.userId)  {
-      return response.status(400).json({ error: 'userId is required' })
+    if (!user) {
+      return response.status(400).json({ error: 'userId missing or invalid' })
     }
 
     const newBlog = {
@@ -24,13 +41,12 @@ blogsRouter.post('/', async (request, response, next) => {
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
-      user: body.userId
+      user: user._id,
     }
 
     const blog = new Blog(newBlog)
 
     const savedBlog = await blog.save()
-    const user = await User.findById(body.userId)
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
     response.status(201).json(savedBlog)
